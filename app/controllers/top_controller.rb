190d9bfef8edda4f138b78b1d7234bc4e74ee9af
masaya <h.masaya17@gmail.com>
2020-05-31 21:10:04 +0000

    #35 検索機能(ransack)追加完了

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,6 @@
 class TopController < ApplicationController
   def index
-    @posts = Post.order(created_at: "DESC").page(params[:page]).per(9)
+    @search = Post.search(params[:q])
+    @posts = @search.result.order(created_at: "DESC").page(params[:page]).per(9)
   end
 end
