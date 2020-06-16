25bfcf1ef1d7a2c2b78cf10354a5476aee0654b2
masaya <h.masaya17@gmail.com>
2020-06-16 21:17:05 +0000

    #62

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,14 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
+  config.fog_provider = 'fog/aws' # required
   config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['KEY'],  # required
-    aws_secret_access_key: ENV['SECRET_KEY'],     # required
-    region:                'ap-northeast-1',                   # optional, defaults to 'us-east-1' オハイオ＝us-east-2
-    #host:                  's3.example.com',             # optional, defaults to nil
-    #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    provider: 'AWS', # required
+    aws_access_key_id: ENV['KEY'], # required
+    aws_secret_access_key: ENV['SECRET_KEY'], # required
+    region: 'ap-northeast-1' # optional, defaults to 'us-east-1' オハイオ＝us-east-2
+    # host:                  's3.example.com',             # optional, defaults to nil
+    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = 'bucket-for-ramenmaster'                # required
+  config.fog_directory  = 'bucket-for-ramenmaster' # required
   config.fog_public     = false                                                 # optional, defaults to true
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
 end
