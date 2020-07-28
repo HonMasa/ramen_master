a9b9a301fb7bcc8699aa56ebc35f9346474029d9
Gitに登録している名前 <Git>
2020-07-28 20:37:27 +0900

    production.rbを修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 server '18.178.206.235', user: 'masaya', roles: %w[app db web]
 
 set :ssh_options, {
-  keys: %w[~/.ssh/ramen_master_key_rsa],
+  keys: %w[~/.ssh/id_rsa_64a5d5dc33b4d617dd1ca8a705a7be77],
   forward_agent: true
 }
