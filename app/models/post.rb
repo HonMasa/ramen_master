e365c8f4e6e422bed6a9de8881f46ee2eed83101
Gitに登録している名前 <Git>
2020-08-19 17:36:55 +0900

    種類を入力必須に

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,7 @@ class Post < ApplicationRecord
   validates :ramen_name, presence: true
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 250 }
+  validates :ramen_kind, presence: true
   geocoded_by :address
   after_validation :geocode
 
