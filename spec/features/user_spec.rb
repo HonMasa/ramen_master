d2eab0f9212ac3eaf905e976b0c61f507be49f13
Gitに登録している名前 <Git>
2020-08-20 18:19:20 +0900

    会員登録の統合テストを追加

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature 'Users', type: :feature do
+  pending "add some scenarios (or delete) #{__FILE__}"
+  # 新しいユーザーを作成する
+  scenario 'creates a new user' do
+    visit root_path
+
+    expect do
+      click_link '会員登録'
+      fill_in '名前', with: 'test_user'
+      fill_in 'メールアドレス', with: 'test@example.com'
+      fill_in 'パスワード', with: 'password'
+      fill_in 'パスワード確認', with: 'password'
+      click_button '登録'
+
+      expect(page).to have_content 'アカウントを登録しました。'
+    end.to change(User.count).by(1)
+  end
+end
