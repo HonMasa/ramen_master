7877cf80054a8f54ce859e58c935b21eee96c34e
Gitに登録している名前 <Git>
2020-08-20 17:52:04 +0900

    投稿に入れる情報の名前を変更

---
+++ spec/features/posts_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature 'Posts', type: :feature do
+  pending "add some scenarios (or delete) #{__FILE__}"
+
+  # ユーザーは新しい投稿を作成する
+  scenario 'user creates a new project' do
+    user = FactoryBot.create(:user)
+
+    visit root_path
+    click_link 'ログイン'
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: user.password
+    click_button 'ログイン'
+
+    expect do
+      click_link '新規投稿'
+      fill_in 'ラーメン名', with: 'ramen_name'
+      fill_in 'ラーメンの種類', with: '醤油'
+      fill_in '店名', with: 'ramen_shop'
+      fill_in '評価', with: '2'
+      fill_in '都道府県', with: '13'
+      fill_in '住所', with: '東京'
+      click_button '投稿'
+
+      expect(page).to have_content '記事を投稿しました！'
+      expect(page).to have_content 'ramen_name'
+    end.to change(user.projects, :count).by(1)
+  end
+end
