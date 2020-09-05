2644d612281980760bd03ad6dca7f09934b6ef50
Gitに登録している名前 <Git>
2020-09-05 18:31:59 +0900

    rubocop -aを実行

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,15 +1,14 @@
 Geocoder.configure(
 
-    # street address geocoding service (default :nominatim)
-    lookup: :google,
+  # street address geocoding service (default :nominatim)
+  lookup: :google,
 
-    # to use an API key:
-    api_key: "AIzaSyB3JzJHRmEDm1vQXU-WUmh4AsN4aTc1fGc",
+  # to use an API key:
+  api_key: 'AIzaSyB3JzJHRmEDm1vQXU-WUmh4AsN4aTc1fGc',
 
-    # geocoding service request timeout, in seconds (default 3):
-    timeout: 5,
+  # geocoding service request timeout, in seconds (default 3):
+  timeout: 5,
 
-    # set default units to kilometers:
-    units: :km
-
+  # set default units to kilometers:
+  units: :km
 )
