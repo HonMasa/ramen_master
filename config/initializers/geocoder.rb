0f9dccf8c8e85014dfe006120d4ee38614ec17f3
本田雅哉 <Git>
2020-09-16 19:29:48 +0900

    APIのキーを非公開に

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -4,7 +4,7 @@ Geocoder.configure(
   lookup: :google,
 
   # to use an API key:
-  api_key: Rails.application.credentials.google[:google_api_key]
+  api_key: Rails.application.credentials.google[:google_api_key],
 
   # geocoding service request timeout, in seconds (default 3):
   timeout: 5,
